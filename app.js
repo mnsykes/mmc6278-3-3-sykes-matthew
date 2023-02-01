8766fa0559fbb71b7fa46043101db093efffb607
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2023-01-31 22:40:00 -0500

    completed

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/api/city/:city", async (req, res) => {
+	console.log(req.params);
 	try {
-		let jobs = await getJobs(req);
+		let jobs = await getJobs(req.params["city"]);
 		let cityInfo = await getCityInfo(req);
 		if (!cityInfo && !jobs) return res.status(404).json({ error: "Data not found" });
 		res.json({ jobs, cityInfo });
 	} catch (err) {
 		return res.status(404).json({
-			error: "Data not found"
+			error: "Data not found."
 		});
 	}
 });
